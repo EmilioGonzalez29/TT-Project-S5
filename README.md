eeba92966b5f14ae48fe6e7b10564c8f71a809f7
Egonzalez <egonzalezravanelli@gmail.com>
2024-02-13 16:09:54 -0300

    version 2.0

--- README.md
+++ README.md
@@ -1,19 +1,14 @@
 # TT-Project-S5
 TripleTen Data Analyst bootcamp - Sprint 5 project - Creation of a web application
   
-##Versión 1.00:  
-###Creación del entorno virtual e instalación de paquetes  
-Se crea el entorno virtual local "cars_env"  
-  
-Se instalan en el entorno virtual las librerías:
-- Pandas
-- Streamlit
-- Plotly-express
-  
-Se crea el archivo requirements.txt con las librerías usadas en el entorno virtual  
-  
-###Análisis exploratorio de datos  
-Se realiza un análisis preliminar del dataset, generando algunos gráficos de visualización en un archivo de Jupyter Notebook, EDA.ipynb, con el objetivo de tener un panorama general de los datos.
+## Versión 2.00:  
+La presente aplicación analiza un dataset de venta de vehículos.  
+Se divide en dos apartados:  
 
-###Creación de la aplicación con Streamlit  
-Se genera el archivo app.py, donde se especifica el código fuente de la aplicación creada
+### Tablas resumen:
+Aquí se puede acceder por medio de botones a tablas donde se resumen los veinte vehículos de mayor precio, según la condición de los mismos: nuevo, excelente, bueno.
+  
+### Visualización gráfica de datos:
+Aquí, por medio de cajas de verificación, se puede acceder a diferentes gráficos donde se describen:
+* Análisis de precios de vehículos en venta: por medio de histograma y gráfico de burbujas podemos realizar diferentes comparaciones que tienen que ver con los precios de los vehículos en venta en función de la condición del vehículo, año del modelo, y kilometraje actual.  
+* Análisis de características de vehículos: por medio de histogramas se obtiene la distribución de modelos de vehículos en venta según tipo de transmisión, y distribución de tipo de vehículo según el tipo de combustible que usa.
--- app.py
+++ app.py
@@ -1,34 +1,100 @@
 import pandas as pd
 import plotly.express as px
+import plotly.figure_factory as ff
 import streamlit as st
 
 car_data = pd.read_csv('./Dataset/vehicles_us.csv') # leer los datos
 
-st.header('Análisis de datos para anuncios de venta de vehículos')
-#hist_button = st.button('Crear histograma') #Creación de botón
+st.title('Análisis de datos para anuncios de venta de vehículos')
+
+st.header('Tablas resumen')
+
+#Filtrado de tablas para resumir datos
+cars_new = car_data[car_data['condition'] == 'new'].sort_values(by='price', ascending=False).reset_index(drop=True)[:20]
+cars_excellent = car_data[car_data['condition'] == 'excellent'].sort_values(by='price', ascending=False).reset_index(drop=True)[:20]
+cars_good = car_data[car_data['condition'] == 'good'].sort_values(by='price', ascending=False).reset_index(drop=True)[:20]
+
+#Creación botones para tablas
+build_table_new = st.button('Los primeros 20 vehículos en estado "nuevo" de mayor precio')
+build_table_excellent = st.button('Los primeros 20 vehículos en estado "excelente" de mayor precio')
+build_table_good = st.button('Los primeros 20 vehículos en estado "bueno" de mayor precio')
+
+#Representación de tablas
+if build_table_new:
+    #Creación de tabla
+    fig = ff.create_table(cars_new)
+    #Representación de tabla
+    fig.show()
+
+if build_table_excellent:
+    #Creación de tabla
+    fig = ff.create_table(cars_excellent)
+    #Representación de tabla
+    fig.show()
+
+if build_table_good:
+    #Creación de tabla
+    fig = ff.create_table(cars_good)
+    #Representación de tabla
+    fig.show()
+
+
+
+st.header('Visualización gráfica de datos')
+
+#Se reemplazan valores ausentes en "model_year" y "odometer" solo para efectos del procesamiento gráfico
+#Para "model_year" se utiliza el año promedio de todo el dataset para los valores ausentes, ya que se desconoce la fecha del mismo
+#Para "odometer", se coloca en 0 para los valores ausentes
+#Se asigna a un nuevo DataFrame, para no tener dichos cambios reflejados en el dataset original
+car_data_graph = car_data
+car_data_graph['model_year'] = car_data_graph['model_year'].fillna(car_data_graph['model_year'].mean().astype('int'))
+car_data_graph['odometer'] = car_data_graph['odometer'].fillna(0)
+
+st.subheader('Análisis de precios de vehículos en venta')
 
 # crear casillas de verificación
-build_histogram = st.checkbox('Construir un histograma')
-build_scatter = st.checkbox('Construir un diagrama de dispersión')
+build_histogram_1 = st.checkbox('Histograma')
+build_bubble = st.checkbox('Gráfico de burbujas')
 
-if build_histogram:  # si la casilla de verificación está seleccionada
-    # escribir un mensaje
-    st.write('Creación de un histograma para el kilometraje de los coches en venta anunciados')
-            
+if build_histogram_1:  # si la casilla de verificación está seleccionada          
+    #Escribir mensaje
+    st.write('Histograma de precios según condición del vehículo')
+    
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data_graph, x="price", color='condition')
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-if build_scatter:  # si la casilla de verificación está seleccionada
+if build_bubble:  # si la casilla de verificación está seleccionada
     # escribir un mensaje
-    st.write('Creación de un diagrama de dispersión para el kilometraje vs precio de los coches')
+    st.write('Diagrama de burbujas para el precio versus el año del modelo, según condición del vehículo')
+    st.write('Nota: El tamaño de burbuja es proporcional al kilometraje del vehículo')
             
-    # crear un diagrama de dispersión
-    fig = px.scatter(car_data, x="odometer", y="price", color='price')
+    # crear un diagrama de burbuja
+    fig = px.scatter(car_data_graph, x="model_year", y="price", size='odometer' , color='condition')
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
-            
 
+st.subheader('Análisis de características de vehículos en venta')
+
+# crear casillas de verificación
+build_histogram_2 = st.checkbox('Histograma de modelos de vehículos en venta')
+build_histogram_3 = st.checkbox('Histograma de tipo de vehículo')
+
+if build_histogram_2:
+    #Escribir un mensaje
+    st.write('Distribución de modelos de vehículos en venta según tipo de transmisión')
+
+    fig = px.histogram(car_data_graph, x="model", color='transmission')
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if build_histogram_3:
+    #Escribir un mensaje
+    st.write('Distribución del tipo de vehículos en venta según el tipo de combustible')
+
+    fig = px.histogram(car_data_graph, x="type", color='fuel')
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
